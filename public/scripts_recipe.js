fcb6b3829c557438e0d4d2550ad116f6b3d77871
mcoutinho88 <matheuscavalcante88@hotmail.com>
2020-02-05 19:17:11 -0300

    second version | modified images, added button for recipe descriptions

--- public/scripts_recipe.js
+++ public/scripts_recipe.js
@@ -1,6 +1,12 @@
 const ingredientContent = document.querySelector('.ingrediente-content');
 const ingredientShowHide = document.getElementById("ingrediente")
 
+const preparoContent = document.querySelector('.preparo-content');
+const preparoShowHide = document.getElementById("preparo")
+
+const infoContent = document.querySelector('.info-content');
+const infoShowHide = document.getElementById("info")
+
 ingredientShowHide.onclick = function(){
     console.log("button pressed")
     if(ingredientContent.style.display == "none"){
@@ -11,6 +17,34 @@ ingredientShowHide.onclick = function(){
     {
         ingredientContent.style.display = "none"
         ingredientShowHide.textContent = "Mostrar"
+
+    }
+}
+
+preparoShowHide.onclick = function(){
+    console.log("button pressed")
+    if(preparoContent.style.display == "none"){
+        preparoContent.style.display = "block"
+        preparoShowHide.textContent = "Esconder"
+    }
+    else
+    {
+        preparoContent.style.display = "none"
+        preparoShowHide.textContent = "Mostrar"
+
+    }
+}
+
+infoShowHide.onclick = function(){
+    console.log("button pressed")
+    if(infoContent.style.display == "none"){
+        infoContent.style.display = "block"
+        infoShowHide.textContent = "Esconder"
+    }
+    else
+    {
+        infoContent.style.display = "none"
+        infoShowHide.textContent = "Mostrar"
 
     }
 }
